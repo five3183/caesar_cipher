df19741821f629013a3bf126740b5c49aa1df9ce
Matthew Curran <five3183@gmail.com>
2019-04-11 12:12:07 -0400

    cipher

---
+++ coder.py
@@ -0,0 +1,40 @@
+import string
+alpha = string.ascii_lowercase
+
+def code_this_word(key, word):
+    if key > 25:
+        key = key-25
+    offset = alpha[key::]
+    offset = offset + alpha[:key:]
+    code_word = ''
+    for letter in word:
+        if letter == ' ':
+            letter == ' '
+            code_word = code_word + letter     
+        else:
+            position = alpha.find(letter.lower())
+            code_word = code_word + offset[position]
+    return code_word
+
+def decode_this_word(key, word):
+    if key > 25:
+        key = key-25
+    offset = alpha[key::]
+    offset = offset + alpha[:key:]
+    decode_word = ''
+    for letter in word:
+        if letter == ' ':
+            letter == ' '
+            decode_word = decode_word + letter    
+        else:
+            position = offset.find(letter.lower())
+            decode_word = decode_word + alpha[position]
+    return decode_word
+
+
+test1 = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG'
+
+test2 = code_this_word(23, test1)
+
+print('Coded word: ' + code_this_word(23, test1))
+print('Decoded word: ' + decode_this_word(23, test2))
